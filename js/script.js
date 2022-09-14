c65466f196795b88fa0ed758c1274e2149149fb9
Nathan Schreiber <nathanschreiber76@gmail.com>
2022-09-14 09:52:51 -0400

    Add calculator functionality
    Created the ability to use the calculator for basic math equations.

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const add = document.querySelector('#add');
 const subtract = document.querySelector('#subtract');
 const multiply = document.querySelector('#multiply');
 const divide = document.querySelector('#divide');
+const operatorButtons = document.querySelectorAll('.operator');
 
 const inputs = document.querySelectorAll('.inputs');
 const one = document.querySelector('#one');
@@ -25,35 +26,34 @@ clearButton.addEventListener('click', () => {
     total = 0;
     screen.textContent = 0;
     decimal.style.pointerEvents = "auto";
+    firstNumber = "";
+    secondNumber = "";
 });
 
 // Function to use specific operator depending on what's clicked
-const operate = function(x, y) {
-    add.addEventListener('click', () => {
-        total = x + y;
-    });
-    subtract.addEventListener('click', () => {
-        total = x - y;
-    });
-    multiply.addEventListener('click', () => {
-        total = x * y;
-    });
-    divide.addEventListener('click', () => {
-        total = x / y;
-    });
-    equal.addEventListener('click', () => {
-        screen.textContent = `${total}`;
-    });
-};
+// const operate = function(x, y) {
+//     add.addEventListener('click', () => {
+//         total = x + y;
+//     });
+//     subtract.addEventListener('click', () => {
+//         total = x - y;
+//     });
+//     multiply.addEventListener('click', () => {
+//         total = x * y;
+//     });
+//     divide.addEventListener('click', () => {
+//         total = x / y;
+//     });
+//     equal.addEventListener('click', () => {
+//         screen.textContent = `${total}`;
+//     });
+// };
 
-// one.addEventListener('click', (e) => {
-//     x = e.target.textContent;
-//     screen.textContent = `${x}`;
-// });
+let newInput = 0;
 
 // Makes numbers/decimal appear on screen as one "number" (string)
 for (let i = 0; i < inputs.length; i++) {
-    inputs[i].addEventListener("click", function(e) {
+    inputs[i].addEventListener('click', function(e) {
       if (screen.textContent == 0) {
         x = e.target.textContent;
         if (x == ".") { // Disallows multiple decimal points
@@ -71,3 +71,40 @@ for (let i = 0; i < inputs.length; i++) {
       screen.textContent = `${newInput}`;
     });
 };
+
+// Stores first input in variable after operator is pressed / stores operator
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
+operatorButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        operator = e.target.textContent;
+        if (firstNumber == "") {
+            firstNumber = newInput;
+            screen.textContent = 0;
+            newInput = 0;
+        } else {
+            screen.textContent = 0; // Clears screen for each number after first 2
+        }
+        
+        
+    });
+});
+
+//
+const operate = equal.addEventListener('click', (x, y) => {
+    secondNumber = newInput;
+    x = Number(firstNumber);
+    y = Number(secondNumber);
+    if (operator == "x") {
+        total = x * y;
+    } else if (operator == "/") {
+        total = x / y;
+    } else if (operator == "+") {
+        total = x + y;
+    } else if (operator == "-") {
+        total = x - y;
+    };
+    screen.textContent = `${total}`;
+    firstNumber = total;
+});
